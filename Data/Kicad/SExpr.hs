ab628eddb7b3fe8ec27d1556e1c4661ea8da14e6
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2014-10-29 18:16:28 +0000

    Add better AtomDbl printing

--- Data/Kicad/SExpr.hs
+++ Data/Kicad/SExpr.hs
@@ -21,10 +21,13 @@ instance Writable SExpr where
                         || head atm `elem` '.':'-':['0'..'9']
                         || foldr
                             (\c z -> z || c `elem` ')':'(':'\\':'\"':['\0'..' '])
-                                False atm
-                                       = show atm -- escaped string with quotes
-                        | otherwise    = atm
-    write (AtomDbl atm) = show atm
+                                False atm = show atm -- escaped string with quotes
+                        | otherwise       = atm
+    -- this should just be printf "%g" but that doesn't work as it should
+    write (AtomDbl atm) = strip_zeros $ break (== '.') $ show atm
+        where strip_zeros (s1,s2) = s1 ++ (dot_if_needed $ reverse
+                                               $ dropWhile (=='0') $ reverse s2)
+              dot_if_needed s     = if s == "." then "" else s
     write (List    sxs) = write sxs
 
 instance Writable [SExpr] where
