f23b159a7059f37ea660c9050a4363f313f7098d
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-12-19 19:27:47 +0000

    Remove SExpr test

--- tests/SExpr.hs
+++ tests/SExpr.hs
@@ -13,29 +13,8 @@ import Utils
 import Data.Kicad.SExpr
 
 tests :: [Test]
-tests = [ testProperty "parse all keywords" parseAllKeywords
+tests = [
         ]
 
-parseAllKeywords :: Keyword -> Bool
-parseAllKeywords kw = tracedPropEq t1 t2
-    where t1 = parse ("(" ++  writeKeyword kw ++ ")")
-          t2 = parse $ either id write $ parse ("(" ++  writeKeyword kw ++ ")")
-
 instance Arbitrary SExpr where
-    arbitrary = oneof [ liftM AtomKey arbitrary
-                      , liftM AtomStr genSafeString
-                      , liftM AtomDbl arbitrary
-                      , do ls <- arbitrary
-                           kw <- elements specialKeywords
-                           return $ List $ [AtomKey kw, AtomStr "a", AtomStr "b", AtomStr "c"] ++ ls
-                      , do ls <- arbitrary
-                           kw <- arbitrary
-                           return $ List $ AtomKey kw : ls
-                      ]
-
-instance Arbitrary Keyword
-    where arbitrary = elements $ filter notSpecial [minBound .. maxBound]
-            where notSpecial x = x `notElem` specialKeywords
-
-specialKeywords :: [Keyword]
-specialKeywords = [KeyTags, KeyModule, KeyPad, KeyFpText, KeyDescr, KeyTedit]
+    arbitrary = undefined
