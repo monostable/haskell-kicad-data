64349d4e1c5e85996ce4c08ca833be1d559b6b48
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-01-01 17:23:45 +0100

    Tweak comments and refactor in SExpr/Write.hs

--- Data/Kicad/SExpr/Write.hs
+++ Data/Kicad/SExpr/Write.hs
@@ -9,17 +9,15 @@ import Text.PrettyPrint.Compact
 
 import Data.Kicad.SExpr.SExpr
 
-{-| Pretty-print a 'SExpr' as a readable 'Doc'. -}
+{-| Pretty-print an s-expression as a readable "document". -}
 pretty :: SExpr -> Doc
 pretty (List _ xs) = text "(" <> align (sep $ map pretty xs) <> text ")"
 pretty atm = text $ write atm
 
 
-{-| Serialize an SExpr as a compact s-expression 'String'. -}
+{-| Serialize an s-expression into a compact string. -}
 write :: SExpr -> String
-write (Atom _ str) |  (str == "")
-                       || foldr
-                           (\c z -> z || c `elem` ')':'(':'\\':'\"':['\0'..' '])
-                               False str = show str -- escaped string with quotes
-                   | otherwise       = str
+write (Atom _ str) | (str == "") || needs_quotes = show str -- escaped string with quotes
+                   | otherwise                   = str      -- bare string without quotes
+    where needs_quotes = foldr (\c z -> z || c `elem` ')':'(':'\\':'\"':['\0'..' ']) False str
 write (List _ sxs) = "(" ++ unwords (map write sxs) ++ ")"
